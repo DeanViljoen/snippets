fa5202b828f5e2bf6b4f1b2d61ed21c7c7a0f098
Dean Viljoen <ddv.rsa@gmail.com>
2023-11-10 21:41:40 +0200

    comments expanded

--- console logging.js
+++ console logging.js
@@ -1,4 +1,5 @@
 //Running directly from VS Code using the Code Runner extension
+//this feels less like VS and more like freeCodeCamp :P
 let myName = "Dean";
 let myLastName = "Viljoen";
 console.log(myName,myLastName);
