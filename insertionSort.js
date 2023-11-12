d13c9fb19c6478c5dc634c11481abd1a7247ccfb
Dean Viljoen <ddv.rsa@gmail.com>
2023-11-12 17:03:08 +0200

    sorting practice

---
+++ insertionSort.js
@@ -0,0 +1,20 @@
+let arr = [5,3,2,9,1,4,6,7,8,0];
+
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let temp = arr[i];
+        let j = i - 1;
+        //loop through elements before / left of index i
+        //if it is GREATER than temp, move it one place up
+        //whatever is in 'temp' will go in the 'opening'
+        //if nothing GREATER than temp is found, temp will 'stay' at position i
+        while (j >= 0 && arr[j] > temp) {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp; //j tracks where temp should go;
+        console.log("Iteration",i,":",arr);
+    }
+    return arr;
+}
+console.log(insertionSort(arr));
--- selectionSort.js
+++ selectionSort.js
@@ -1,3 +1,4 @@
+/*
 function selectionSort(arr) {
     let min = 0;
     for (let i = 0; i < arr.length - 1; i++) { //final element read is 2nd last element of arr
@@ -13,6 +14,22 @@ function selectionSort(arr) {
     }
     return arr;
 }
+*/
+//12-11-23: Let's try implementing selection sort again!
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        let min = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[min] > arr[j]) {
+                min = j;
+            }
+        }
+        let temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+    return arr;
+}
 
-let arr = [5,3,2,9,6,8,1];
+let arr = [5,3,2,9,6,8,1,4,0,7];
 console.log(selectionSort(arr));
